fc263e858a6ecb3532f6d3191afc14edc1007fc4
Foo Bar <124519337+Ulduz23@users.noreply.github.com>
2023-10-27 11:58:15 +0400

    newsdetailres change

--- app/Http/Resources/Api/v1/News/NewsDetailResource.php
+++ app/Http/Resources/Api/v1/News/NewsDetailResource.php
@@ -14,12 +14,7 @@ class NewsDetailResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
-            'id' => $this->id,
-            'title' => $this->title,
-            'description' => $this->description,
-            'view_count' => $this->view_count,
-            'created_at' => date('Y-m-d H:i:s',strtotime($this->created_at)),
-        ];
+        return parent::toArray($request);
+
     }
 }
